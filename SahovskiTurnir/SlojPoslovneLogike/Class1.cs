ad301a6b18d313f40ead56f1dea1ce6fdada79ae
Babuta3 <urkecar2003@gmail.com>
2026-06-11 23:50:49 +0200

    PromenaStrukture
    zamenio koje class libraries projekte koristim

--- SahovskiTurnir/SlojPoslovneLogike/Class1.cs
+++ SahovskiTurnir/SlojPoslovneLogike/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SlojPoslovneLogike
+﻿namespace SlojPoslovneLogike
 {
     public class Class1
     {
+
     }
 }
