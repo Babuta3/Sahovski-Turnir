2df30d93d2af259c386e6116922f8bea5c10bf1a
Babuta3 <urkecar2003@gmail.com>
2026-06-12 00:17:54 +0200

    Konekcija dodata

---
+++ SahovskiTurnir/SlojPodataka/TehnoloskeKlase/Konekcija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlojPodataka.TehnoloskeKlase
+{
+    public class Konekcija
+    {
+        public static string NizKonekcije { get; set; } = string.Empty;
+    }
+}
