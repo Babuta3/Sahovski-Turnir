7751fb04d800374838263ddcbdb02a5f3a49fd7f
Babuta3 <urkecar2003@gmail.com>
2026-06-12 00:08:58 +0200

    Ubacen Plasman u Turnir i Igrac

--- SahovskiTurnir/SlojPodataka/KlasePodataka/Igrac.cs
+++ SahovskiTurnir/SlojPodataka/KlasePodataka/Igrac.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SlojPodataka.KlasePodataka
 {
-    internal class Igrac
+    [Table("Igrac")]
+    public class Igrac : OsnovniEntitet
     {
+        [Key]
+        public int IgracID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Ime { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Prezime { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Klub { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10)]
+        public string Titula { get; set; } = "Bez titule";
+
+        public int? ELO { get; set; }
+
+        public ICollection<PlasmanIgraca> Plasmani { get; set; } = new List<PlasmanIgraca>();
     }
 }
