1bbb7ba782ae26eab84f94e8e63165bf3abcade0
Babuta3 <urkecar2003@gmail.com>
2026-06-12 23:14:44 +0200

    Dodat IgracViewModel.cs u ViewModels

---
+++ SahovskiTurnir/PrezentacioniSloj/PrezentacionaLogika/ViewModels/IgracViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrezentacioniSloj.PrezentacionaLogika.ViewModels
+{
+    public class IgracViewModel
+    {
+        public int IgracID { get; set; }
+
+        [Required(ErrorMessage = "Ime je obavezno.")]
+        [StringLength(50)]
+        [Display(Name = "Ime")]
+        public string Ime { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Prezime je obavezno.")]
+        [StringLength(50)]
+        [Display(Name = "Prezime")]
+        public string Prezime { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Klub je obavezan.")]
+        [StringLength(100)]
+        [Display(Name = "Klub")]
+        public string Klub { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Titula je obavezna.")]
+        [Display(Name = "Titula")]
+        public string Titula { get; set; } = string.Empty;
+
+        [Display(Name = "ELO rejting")]
+        [Range(100, 3000, ErrorMessage = "ELO mora biti između 100 i 3000.")]
+        public int? ELO { get; set; }
+    }
+}
