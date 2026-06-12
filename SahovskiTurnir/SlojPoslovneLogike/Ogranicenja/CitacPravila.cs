06cd95d3d606688e4a45e3836e2821052abc7ac9
Babuta3 <urkecar2003@gmail.com>
2026-06-12 22:30:33 +0200

    Dodati Folderi i CitacPravila

---
+++ SahovskiTurnir/SlojPoslovneLogike/Ogranicenja/CitacPravila.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlojPoslovneLogike.Ogranicenja
+{
+    public class CitacPravila
+    {
+        private readonly HttpClient _httpKlijent;
+        private const string _urlServisa = "http://localhost:5072/api/PravilaRest";
+
+        public CitacPravila(HttpClient httpKlijent)
+        {
+            _httpKlijent = httpKlijent;
+        }
+
+        public async Task<int> DohvatiBrojNagradjeniхMesta()
+        {
+            var pravila = await _httpKlijent.GetFromJsonAsync<PravilaModel>(_urlServisa);
+            return pravila?.BrojNagradjeniхMesta ?? 3;
+        }
+    }
+
+    public class PravilaModel
+    {
+        public int BrojNagradjeniхMesta { get; set; }
+    }
+}
