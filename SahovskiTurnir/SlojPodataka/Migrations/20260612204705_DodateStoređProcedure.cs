462f17914ba9efa1ea163fc4bf8aac00c7b0da42
Babuta3 <urkecar2003@gmail.com>
2026-06-12 22:49:37 +0200

    Dodate stored procedure kroz EF migraciju

---
+++ SahovskiTurnir/SlojPodataka/Migrations/20260612204705_DodateStoređProcedure.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SlojPodataka.Migrations
+{
+    /// <inheritdoc />
+    public partial class DodateStoređProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE sp_DajUkupanBrojTurnira
+                AS
+                BEGIN
+                    SELECT COUNT(*) FROM Turnir
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE sp_DajUkupanBrojIgraca
+                AS
+                BEGIN
+                    SELECT COUNT(*) FROM Igrac
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS sp_DajUkupanBrojTurnira");
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS sp_DajUkupanBrojIgraca");
+        }
+    }
+}
