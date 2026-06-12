ba7bf4ffe3e44824dcae3c4af3661d161aa831b5
Babuta3 <urkecar2003@gmail.com>
2026-06-12 13:34:41 +0200

    Napraveljena klasa PravilaRestKontroler

---
+++ SahovskiTurnir/SlojServisa/WebServis/PravilaRestKontroler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace SlojServisa.WebServis
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PravilaRestKontroler : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult DohvatiPravila()
+        {
+            try
+            {
+                var putanja = Path.Combine(AppContext.BaseDirectory, "Ogranicenja", "pravila_nagrade.xml");
+                var xml = XDocument.Load(putanja);
+
+                return Ok(new
+                {
+                    BrojNagradjeniхMesta = int.Parse(xml.Root!.Element("BrojNagradjeniхMesta")!.Value),
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Greška pri čitanju XML-a: {ex.Message}");
+            }
+        }
+    }
+}
