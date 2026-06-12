4b060c7083800fb18b5b3e823e874c543ee4fe89
Babuta3 <urkecar2003@gmail.com>
2026-06-12 13:04:00 +0200

    napravljen IgracDTO

---
+++ SahovskiTurnir/SlojServisa/DTO/IgracDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SlojServisa.DTO
+{
+    public class IgracDTO
+    {
+        public int IgracID { get; set; }
+        public string Ime { get; set; } = string.Empty;
+        public string Prezime { get; set; } = string.Empty;
+        public string Klub { get; set; } = string.Empty;
+        public string Titula { get; set; } = string.Empty;
+        public int? ELO { get; set; }
+    }
+}
