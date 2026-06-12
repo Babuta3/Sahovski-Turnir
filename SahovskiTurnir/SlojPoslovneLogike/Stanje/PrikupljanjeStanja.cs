68fc34e099facaf955369e7cb1c2348aca4ddfa2
Babuta3 <urkecar2003@gmail.com>
2026-06-12 22:31:10 +0200

    Dodata klasa PrikupljanjeStanja u Stanje

---
+++ SahovskiTurnir/SlojPoslovneLogike/Stanje/PrikupljanjeStanja.cs
@@ -0,0 +1,26 @@
+﻿using SlojPodataka.TehnoloskeKlase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlojPoslovneLogike.Stanje
+{
+    public class PrikupljanjeStanja
+    {
+        private readonly TurnirRepozitorijum _repozitorijum;
+
+        public PrikupljanjeStanja(TurnirRepozitorijum repozitorijum)
+        {
+            _repozitorijum = repozitorijum;
+        }
+
+        public decimal DohvatiNagradniFond(int turnirId)
+        {
+            var turnir = _repozitorijum.DohvatiPoId(turnirId);
+            if (turnir == null) throw new Exception("Turnir nije pronađen.");
+            return turnir.NagradniFond;
+        }
+    }
+}
