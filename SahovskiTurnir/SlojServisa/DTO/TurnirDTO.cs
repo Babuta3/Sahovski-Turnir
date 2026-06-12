9fc2d61a6a3a723e82adcb8cecf72662ee875e71
Babuta3 <urkecar2003@gmail.com>
2026-06-12 12:58:16 +0200

    Dodati Folderi i TurnirDTO klasa

---
+++ SahovskiTurnir/SlojServisa/DTO/TurnirDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace SlojServisa.DTO
+{
+    public class TurnirDTO
+    {
+        public int TurnirID { get; set; }
+        public string NazivTurnira { get; set; } = string.Empty;
+        public string Mesto { get; set; } = string.Empty;
+        public DateTime Datum { get; set; }
+        public string Organizator { get; set; } = string.Empty;
+        public string GlavniArbitar { get; set; } = string.Empty;
+        public decimal NagradniFond { get; set; }
+        public string TipTurnira { get; set; } = string.Empty;
+        public string FormatTakmicenja { get; set; } = string.Empty;
+        public int BrojRundi { get; set; }
+        public int BrojUcesnika { get; set; }
+        public string VremenskaKontrola { get; set; } = string.Empty;
+        public string TiebreakKriterijum { get; set; } = string.Empty;
+    }
+}
