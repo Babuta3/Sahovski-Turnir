0a3b48ad536a2806bacd1be3681cf3d740e245b2
Babuta3 <urkecar2003@gmail.com>
2026-06-12 13:33:03 +0200

    Kreiran KorisnikRestKontroler

---
+++ SahovskiTurnir/SlojServisa/WebServis/KorisnikRestKontroler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlojPodataka.TehnoloskeKlase;
+using SlojServisa.DTO;
+
+namespace SlojServisa.WebServis
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KorisnikRestKontroler : ControllerBase
+    {
+        private readonly KorisnikRepozitorijum _repozitorijum;
+
+        public KorisnikRestKontroler(KorisnikRepozitorijum repozitorijum)
+        {
+            _repozitorijum = repozitorijum;
+        }
+
+        [HttpPost("prijava")]
+        public ActionResult Prijava([FromBody] PrijavaDTO dto)
+        {
+            var korisnik = _repozitorijum.DohvatiPoKorisnickomImenu(dto.KorisnickoIme);
+
+            if (korisnik == null)
+                return Unauthorized("Korisničko ime nije pronađeno.");
+
+            if (!FunkcijeLozinke.Proveri(dto.Lozinka, korisnik.LozinkaHash))
+                return Unauthorized("Lozinka nije ispravna.");
+
+            return Ok("Prijava uspešna.");
+        }
+    }
+}
